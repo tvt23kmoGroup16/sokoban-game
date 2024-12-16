652f472879c40e28a3eb12ef023a472d4e65aece
rahukettu <78539693+rahukettu@users.noreply.github.com>
2024-12-16 19:31:59 +0200

    Add repair to path

--- app/src/main/java/com/example/sokoban/GameMap.kt
+++ app/src/main/java/com/example/sokoban/GameMap.kt
@@ -5,7 +5,6 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.widget.GridLayout
 import android.widget.ImageView
-import com.example.sokoban.gameItems.RandomItemGenerator
 
 
 class GameMap(
